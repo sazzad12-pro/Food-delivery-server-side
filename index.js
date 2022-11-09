21dcb491d079c1dab7742164786da74f2e1c4bed
sazzad <sajusajjad052@gmail.com>
2022-11-09 12:05:37 +0600

    mongobd

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
+
+// ElNoRHp6kts9aOvS
+
+//deliveryService
+
+const uri =
+  "mongodb+srv://<username>:<password>@cluster0.4gwnm.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+  console.log("mongodb connect");
+});
 
 app.get("/", (req, res) => {
   res.send("server is running");
