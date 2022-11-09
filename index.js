6fe74307eecc7bb59138022ffc888625eb819ca4
sazzad <sajusajjad052@gmail.com>
2022-11-09 13:16:10 +0600

    user and password

--- index.js
+++ index.js
@@ -4,31 +4,28 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
+const uri = `mongodb+srv://${process.env.SECRET_USER}:${process.env.SECRET_KEY}@cluster0.4gwnm.mongodb.net/?retryWrites=true&w=majority`;
 
 app.use(cors());
 app.use(express.json());
 
 // ElNoRHp6kts9aOvS
-
 //deliveryService
 
-const uri =
-  "mongodb+srv://<username>:<password>@cluster0.4gwnm.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-  console.log("mongodb connect");
-});
-
-app.get("/", (req, res) => {
-  res.send("server is running");
-});
+async function run() {
+  try {
+    app.get("/", (req, res) => {
+      res.send("server is running");
+    });
+  } finally {
+  }
+}
+run().catch((err) => console.error(err));
 
 app.listen(port, () => {
   console.log(`server is running${port}`);
